27d26f244b099c1027087b27b770fe244028698c
farman1203 <ansarifarman1203@gmail.com>
2026-07-14 17:13:53 +0530

    create a project bookmyshow_clone

---
+++ bookmyshow_clone/Backend/src/app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const movieModel = require("./models/movie.post")
+
+const app = express();
+app.use(express.json())
+
+app.post('/movies', async (req, res) => {
+    const data = req.body;
+
+    const movie = await movieModel.create(data)
+
+    res.status(201).json({
+        message: "Movie add successfully",
+        movie
+    })
+})
+
+app.get("/movies", async (req, res) => {
+    const movie = await movieModel.find()
+
+    return res.status(201).json({
+        message: "Movie fetched Successfully",
+        movie
+    })
+})
+
+
+module.exports = app;
---
+++ bookmyshow_clone/Backend/src/models/movie.post.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const movieSchema = new mongoose.Schema({
+    image : String,
+    name : String,
+    date : String,
+    location : String,
+    action : String
+})
+
+
+const movieModel = mongoose.model("movie",movieSchema)
+
+module.exports=movieModel;
