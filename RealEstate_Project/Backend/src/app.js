c16c5961e96072c997172a9e43454b07ce9aa8ce
farman1203 <ansarifarman1203@gmail.com>
2026-07-27 22:06:28 +0530

    authentication implement

--- RealEstate_Project/Backend/src/app.js
+++ RealEstate_Project/Backend/src/app.js
@@ -1,13 +1,13 @@
-const express = require ('express')
+const express = require('express')
 const authRoutes = require('./routes/auth.routes')
+const cookieParser = require('cookie-parser')
 
 
-const app= express();
-app.use(express.json()); 
+const app = express();
+app.use(express.json());
+app.use(cookieParser())
+
+app.use('/api/auth', authRoutes)
 
 
-
-app.use('/api/auth',authRoutes)
-
-
-module.exports=app;
+module.exports = app;
--- RealEstate_Project/Backend/src/controllers/auth.controller.js
+++ RealEstate_Project/Backend/src/controllers/auth.controller.js
@@ -1,27 +1,56 @@
 const userModel = require('../models/user.model');
 const userSchema = require('../models/user.model')
 const jwt = require('jsonwebtoken');
-const { use } = require('../routes/auth.routes');
-const { json } = require('express');
+const bcrypt = require('bcryptjs')
 
 
 async function registerUser(req, res) {
     const { name, email, password, phone, role } = req.body;
 
+
+    const ifexistinguser = await userModel.findOne({ email })
+
+    if (ifexistinguser) {
+        return res.status(409).json({
+            message: "User already exists"
+        })
+    }
+
+    const hash = await bcrypt.hash(password, 10)
+
     const user = await userModel.create({
-        name, email, password, phone, role
+        name, email, password: hash, phone, role,
     })
 
-    const token = jwt.sign({
-        id: user._id,
-        role: user.role,
-    }, process.env.JWT_SECRET)
+    const token = jwt.sign(
+        {
+            id: user._id,
+            role: user.role,
+        },
+        process.env.JWT_SECRET)
 
-    res.status(201), json({
+    res.cookie("token", token)
+
+    res.status(201).json({
         message: "user created successfully",
         user,
-        token
-    })
+    });
 }
+
+
+// const loginuser = (req,res)=>{
+//     const {email, password} = req.body;
+
+//     const userdata = await userModel.findOne({email});
+
+//     if(!userdata){
+//         return res.status(400).json({
+//             message : "Invalid Credentials"
+//         })
+//     }
+
+//     const 
+// }
+
 
 module.exports = { registerUser }
